203643d0b2db5cd05d2ebf70bbd8ee7cb84aff16
crohasang <hasang2691@naver.com>
2025-01-01 21:43:13 +0900

    feat(Flex, Onboarding): Flex 컴포넌트 생성 및 임시 온보딩 페이지 생성
    - componenets/flex/Flex.tsx에서 Row, Col, Flex 생성
    - Onboarding.tsx에서 임시 온보딩 페이지 생성 후 구글 로그인 버튼 구현(아직 미동작)
    - 구글 로그인 svg 추가

---
+++ src/components/flex/Flex.tsx
@@ -0,0 +1,96 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import React from 'react';
+
+interface FlexProps extends React.HTMLAttributes<HTMLDivElement> {
+  className?: string;
+  justifyContent?: React.CSSProperties['justifyContent'];
+  alignItems?: React.CSSProperties['alignItems'];
+  gap?: React.CSSProperties['gap'];
+  padding?: React.CSSProperties['padding'];
+  margin?: React.CSSProperties['margin'];
+  borderRadius?: React.CSSProperties['borderRadius'];
+  children: React.ReactNode;
+}
+
+export const Row = React.forwardRef<HTMLDivElement, FlexProps>((props, ref) => {
+  const {
+    justifyContent: justify,
+    alignItems: align,
+    borderRadius: radius,
+    children,
+    ...others
+  } = props;
+
+  return (
+    <Flex ref={ref} {...others} justify={justify} align={align} radius={radius} direction="row">
+      {children}
+    </Flex>
+  );
+});
+
+export const Col = React.forwardRef<HTMLDivElement, FlexProps>((props, ref) => {
+  const {
+    justifyContent: justify,
+    alignItems: align,
+    borderRadius: radius,
+    children,
+    ...others
+  } = props;
+
+  return (
+    <Flex ref={ref} {...others} justify={justify} align={align} radius={radius} direction="column">
+      {children}
+    </Flex>
+  );
+});
+
+interface StyledFlexProps {
+  direction: React.CSSProperties['flexDirection'];
+  justify?: React.CSSProperties['justifyContent'];
+  align?: React.CSSProperties['alignItems'];
+  radius?: React.CSSProperties['borderRadius'];
+  padding?: React.CSSProperties['padding'];
+  margin?: React.CSSProperties['margin'];
+  gap?: React.CSSProperties['gap'];
+}
+
+const Flex = styled.div<StyledFlexProps>`
+  display: flex;
+  flex-direction: ${(props) => props.direction};
+  ${({ align }) =>
+    align &&
+    css`
+      align-items: ${align};
+    `}
+  ${({ justify }) =>
+    justify &&
+    css`
+      justify-content: ${justify};
+    `}
+  ${({ gap }) =>
+    gap &&
+    css`
+      gap: ${gap}px;
+    `}
+  width: 100%;
+  ${({ padding }) =>
+    padding &&
+    css`
+      padding: ${padding};
+    `}
+  ${({ margin }) =>
+    margin &&
+    css`
+      margin: ${margin};
+    `}
+  ${({ radius }) =>
+    radius &&
+    css`
+      border-radius: ${radius};
+    `}
+`;
+
+Row.displayName = 'Row';
+Col.displayName = 'Col';
---
+++ src/pages/Onboarding.tsx
@@ -0,0 +1,47 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { Col } from '../components/flex/Flex';
+
+const Onboarding = () => {
+  const handleGoogleLogin = () => {
+    console.log('구글 로그인 버튼 클릭!');
+  };
+
+  return (
+    <Col
+      justifyContent="center"
+      alignItems="center"
+      css={css`
+        height: 100%;
+        width: 100%;
+        position: fixed;
+        top: 0;
+        left: 0;
+      `}
+    >
+      <h1
+        css={css`
+          font-size: 2.5rem;
+          font-weight: bold;
+          margin-bottom: 2rem;
+        `}
+      >
+        먹을래
+      </h1>
+      <img 
+        src="/google_button.svg" 
+        alt="Google Login" 
+        onClick={handleGoogleLogin}
+        css={css`
+          cursor: pointer;
+          transition: transform 0.2s ease;
+          &:hover {
+            transform: scale(1.05);
+          }
+        `}
+      />
+    </Col>
+  );
+};
+
+export default Onboarding;
