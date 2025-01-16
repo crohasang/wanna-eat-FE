7d613007aa897815c5f099ce6fa0817d01762d6a
crohasang <hasang2691@naver.com>
2025-01-17 00:30:07 +0900

    refactor(firebase-messaging-sw.js): service worker 통합
    - 기존의 src/sw.js 삭제
    - public/firebase-messaging-sw.js에 workbox 매니페스트 코드 추가

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,3 +1,6 @@
+// Service Worker에서 사용할 Workbox 매니페스트
+self.__WB_MANIFEST
+
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
