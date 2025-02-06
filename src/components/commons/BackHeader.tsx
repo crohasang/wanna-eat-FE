4aedbe854a37e85ae32638d5727d0490dc50fa9c
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2025-02-06 15:58:34 +0900

    feat: BackHeader
    BackButton을 포함하고 있는 header

---
+++ src/components/commons/BackHeader.tsx
@@ -0,0 +1,35 @@
+import leftArrowImg from '../../assets/leftArrow.svg';
+import { css } from '@emotion/react';
+import { Row } from './Flex';
+
+const BackHeader = ({ title }: { title: string }) => {
+  return (
+    <Row
+      css={css`
+        position: relative;
+        align-items: center;
+        justify-content: center;
+        padding: 12px 0;
+      `}
+    >
+      <img
+        src={leftArrowImg}
+        css={css`
+          position: absolute;
+          left: 20px;
+        `}
+      />
+      <p
+        css={css`
+          font-size: 18px;
+          font-weight: 500;
+          margin: 0;
+        `}
+      >
+        {title}
+      </p>
+    </Row>
+  );
+};
+
+export default BackHeader;
