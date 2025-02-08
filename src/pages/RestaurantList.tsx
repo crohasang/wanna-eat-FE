008a4bafa11d7702b680ff2a2ea35d71584bf1b4
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2025-02-08 17:21:38 +0900

    feat: restaurantList에 homeCard 적용

--- src/pages/RestaurantList.tsx
+++ src/pages/RestaurantList.tsx
@@ -4,13 +4,45 @@ import { useState } from 'react';
 import { css } from '@emotion/react';
 import TagFilter from '../components/restaurantList/TagFilter';
 import SearchBar from '../components/common/SearchBar';
+import HomeCard from '../components/home/HomeCard';
 
 const TAGS = ['가까운', '회식에 딱', '시끌벅적한', '분위기 있는'];
+
+const HomeCardList = [
+  {
+    name: '성수노루',
+    category: '곱창, 막창, 양',
+    address: '서울 성동구 아차산로 110 1층',
+    imageUrls: [
+      'https://www.theborn.co.kr/wp-content/uploads/2017/06/%EC%83%88%EB%A7%88%EC%9D%84%EC%8B%9D%EB%8B%B9_img00.jpg',
+      'https://www.theborn.co.kr/wp-content/uploads/2017/06/%EC%83%88%EB%A7%88%EC%9D%84%EC%8B%9D%EB%8B%B9_img00.jpg',
+    ],
+    tag: ['시끌벅적한', '대규모', '회식에 딱'],
+    isFavorite: true,
+  },
+  {
+    name: '건대치킨',
+    category: '치킨',
+    address: '서울 광진구 능동로 100',
+    imageUrls: [
+      'https://www.theborn.co.kr/wp-content/uploads/2017/06/%EC%83%88%EB%A7%88%EC%9D%84%EC%8B%9D%EB%8B%B9_img00.jpg',
+    ],
+    isFavorite: false,
+  },
+];
 
 const RestaurantList = () => {
   const [search, setSearch] = useState('');
   const [selectedTag, setSelectedTag] = useState<string | null>(null);
+  const [homeCards, setHomeCards] = useState(HomeCardList);
 
+  const toggleHomeCardFavorite = (index: number) => {
+    setHomeCards((prev) =>
+      prev.map((homeCard, i) =>
+        i === index ? { ...homeCard, isFavorite: !homeCard.isFavorite } : homeCard
+      )
+    );
+  };
   return (
     <>
       <Col>
@@ -31,6 +63,20 @@ const RestaurantList = () => {
           상황별로 골라보기
         </h3>
         <TagFilter tags={TAGS} selectedTag={selectedTag} onSelectTag={setSelectedTag} />
+        <Col>
+          {homeCards.map((homeCard, index) => (
+            <HomeCard
+              key={index}
+              name={homeCard.name}
+              category={homeCard.category}
+              address={homeCard.address}
+              tag={homeCard.tag || []}
+              imageUrl={homeCard.imageUrls || []}
+              isFavorite={homeCard.isFavorite}
+              onToggleFavorite={() => toggleHomeCardFavorite(index)}
+            />
+          ))}
+        </Col>
       </Col>
     </>
   );
