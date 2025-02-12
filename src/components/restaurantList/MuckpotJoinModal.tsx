9bef8ddbe509b4916522e03aa7bba34041a5ef84
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2025-02-12 16:21:21 +0900

    feat: MuckpotInfoBox 컴포넌트 분리

--- src/components/restaurantList/MuckpotJoinModal.tsx
+++ src/components/restaurantList/MuckpotJoinModal.tsx
@@ -1,7 +1,6 @@
 import { css } from '@emotion/react';
-import { Col, Row } from '../commons/Flex';
-import TagList from '../home/TagList';
-import RestaurantImg from '../../assets/home/restaurant.svg';
+import { Col } from '../commons/Flex';
+import MuckpotInfoBox from './MuckpotInfoBox';
 
 interface Muckpot {
   id: number;
@@ -22,17 +21,8 @@ interface MuckpotJoinModalProps {
   muckpotData?: Muckpot;
 }
 
-const subTextStyle = css`
-  font-size: 13px;
-  color: #878787;
-  margin-right: 18px;
-  align-self: center;
-  white-space: nowrap;
-`;
-
 const MuckpotJoinModal = ({ isOpen, onClose, muckpotData }: MuckpotJoinModalProps) => {
   if (!isOpen || !muckpotData) return null;
-  const { title, date, time, members, restaurant, tags, category, address, message } = muckpotData;
 
   return (
     <Col
@@ -60,132 +50,14 @@ const MuckpotJoinModal = ({ isOpen, onClose, muckpotData }: MuckpotJoinModalProp
         <h3
           css={css`
             margin-top: 0;
+            text-align: left;
           `}
         >
           현재 다음과 같은 먹팟이 존재해요. <br />
           해당 먹팟에 참여하시겠어요?
         </h3>
 
-        <Col
-          css={css`
-            background: #fff;
-            border-radius: 20px;
-            padding: 16px 16px 0 16px;
-            text-align: left;
-            border: 1px solid #ddd;
-            margin-bottom: 16px;
-          `}
-        >
-          <div
-            css={css`
-              border-bottom: solid 1px #e1e1e1;
-              padding-bottom: 12px;
-            `}
-          >
-            <span
-              css={css`
-                font-weight: 600;
-                color: #3d3d3d;
-              `}
-            >
-              {title}
-            </span>
-            <br />
-            <span
-              css={css`
-                font-size: 14px;
-                font-weight: 400;
-                color: #878787;
-              `}
-            >
-              {date} {time}
-            </span>
-          </div>
-          <Row>
-            <p css={subTextStyle}>구성원</p>
-            <p
-              css={css`
-                font-size: 14px;
-                font-weight: 600;
-                color: #878787;
-              `}
-            >
-              {members.join(' ')}
-            </p>
-          </Row>
-          <Row>
-            <p
-              css={css`
-                font-size: 13px;
-                color: #878787;
-                margin-right: 18px;
-                align-self: center;
-                white-space: nowrap;
-                align-self: flex-start;
-                margin-top: 0;
-              `}
-            >
-              장소
-            </p>
-            <Col
-              css={css`
-                margin-left: 8px;
-                gap: 6px;
-              `}
-            >
-              <Row
-                css={css`
-                  gap: 6px;
-                  font-weight: 600;
-                  color: #3d3d3d;
-                `}
-              >
-                <img
-                  src={RestaurantImg}
-                  alt='restaurantImg'
-                  css={css`
-                    width: 20px;
-                    height: 20px;
-                  `}
-                />
-                {restaurant} <br />
-              </Row>
-              <TagList tags={tags} />
-
-              <span
-                css={css`
-                  font-size: 14px;
-                  font-weight: 400;
-                  color: #3d3d3d;
-                `}
-              >
-                {category}
-              </span>
-
-              <span
-                css={css`
-                  font-size: 13px;
-                  font-weight: 400;
-                  color: #878787;
-                `}
-              >
-                {address}
-              </span>
-            </Col>
-          </Row>
-          <Row>
-            <p css={subTextStyle}>메세지</p>
-            <p
-              css={css`
-                font-size: 16px;
-                font-weight: 400;
-                font-family: 'BM kkubulim';
-              `}
-            >
-              {message}
-            </p>
-          </Row>
-        </Col>
+        <MuckpotInfoBox muckpotData={muckpotData} />
 
         <div
           css={css`
