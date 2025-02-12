04edac1b37ec02d41bd5f1c13e39e06be6655d66
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2025-02-12 16:54:22 +0900

    feat: ComfirmStep

--- src/components/makeGroup/ConfirmStep.tsx
+++ src/components/makeGroup/ConfirmStep.tsx
@@ -1,16 +1,62 @@
-const ConfirmStep = ({ data, nextStep, prevStep }) => {
+import { css } from '@emotion/react';
+import { Col } from '../commons/Flex';
+import Button from '../common/Button';
+import BackHeader from '../commons/BackHeader';
+import MuckpotInfoBox from '../restaurantList/MuckpotInfoBox';
+
+interface ConfirmStepProps {
+  data: {
+    title: string;
+    date: string;
+    time: string;
+    participants: string[];
+    message: string;
+  };
+  nextStep: () => void;
+  prevStep: () => void;
+}
+
+const ConfirmStep = ({ data, nextStep, prevStep }: ConfirmStepProps) => {
   return (
-    <div>
-      <h2>먹팟 등록 전 확인해주세요.</h2>
-      <p>제목: {data.title}</p>
-      <p>
-        일정: {data.date} {data.time}
-      </p>
-      <p>참여자: {data.participants.join(', ')}</p>
-      <p>알림 메시지: {data.message}</p>
-      <button onClick={prevStep}>이전</button>
-      <button onClick={nextStep}>등록하기</button>
-    </div>
+    <Col
+      css={css`
+        align-items: center;
+      `}
+    >
+      <BackHeader firstTitle='먹팟 등록 전 확인해주세요.' alignLeft onBack={prevStep} />
+      <div
+        css={css`
+          width: 90%;
+          margin-top: 20%;
+        `}
+      >
+        <MuckpotInfoBox
+          muckpotData={{
+            title: data.title,
+            date: data.date,
+            time: data.time,
+            members: data.participants,
+            restaurant: '성수노루',
+            tags: ['시끌벅적한', '대규모', '회식에 딱'],
+            category: '곱창, 막창, 양',
+            address: '서울 성동구 아차산로 110 1층',
+            message: data.message,
+          }}
+        />
+      </div>
+
+      <div
+        css={css`
+          width: 90%;
+          display: flex;
+          justify-content: center;
+          position: fixed;
+          bottom: 10px;
+        `}
+      >
+        <Button onClick={nextStep}>다음</Button>
+      </div>
+    </Col>
   );
 };
 
